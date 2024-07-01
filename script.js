17069a69d08f41b308f570ca41b7a2fe842ec57c
madorPele1 <159686722+madorPele1@users.noreply.github.com>
2024-07-01 16:36:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
+let role;
+
 window.addEventListener("load", () => {
     let startButton = document.getElementById("start-button");
     startButton.addEventListener("click", startLomda);
+    document.getElementById('soldier').addEventListener("click", choose);
+    document.getElementById('commander').addEventListener("click", choose);
+    document.getElementById('chosen-button').addEventListener("click", chosen);
 })
 
 const startLomda = () => {
-    document.getElementById('panel').style.animation = 'fade-out 1s forwards';
+    document.getElementById('panel1').style.animation = 'fade-out 1s forwards';
     setTimeout(() => { 
-        document.getElementById('panel').innerHTML = '<p class="instructions">המעבר בין עמוד לעמוד בלומדה יעשה באמצעות גלילה/החלקה מעלה.</p><p class="instructions">ניתן לחזור בכל עת למסלול ולחזור ליחידות הלימוד הקודמות באמצעות לחיצה על המכונית המופיעה בצד שמאל של המסך.</p><p class="instructions bold">גללו מטה כדי להמשיך..</p><p class="down-arrows">︾</p>';
-        document.getElementById('panel').style.animation = 'fade-in 1s forwards';    
+        document.getElementById('panel1').innerHTML = '<h1 class="main-title">השימוש בלומדה</h1><p class="instructions">המעבר בין עמוד לעמוד בלומדה יעשה באמצעות גלילה/החלקה מעלה.</p><p class="instructions">ניתן לחזור בכל עת למסלול ולחזור ליחידות הלימוד הקודמות באמצעות לחיצה על המכונית המופיעה בצד שמאל של המסך.</p><p class="instructions bold">גללו מטה כדי להמשיך..</p><p class="down-arrows">︾</p>';
+        document.getElementById('panel1').style.animation = 'fade-in 1s forwards';    
     }, 1000);
+}
+
+const choose = (event) => {
+    document.getElementById('soldier').classList.remove('picked');
+    document.getElementById('commander').classList.remove('picked');
+    role = event.target.id;
+    document.getElementById(role).classList.add('picked');
+}
+
+const chosen = () => {
+    document.getElementById('animation-car').style.animation = 'back-car-drive 1s linear';
+    setTimeout(() => { 
+        document.getElementById('animation-car').style.animation = 'right-car-drive 1s linear forwards';    
+        document.getElementById('animation-car').setAttribute('src', 'assets/general/cars/sideView.svg');
+    }, 900);
 }
